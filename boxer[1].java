7e5760a62fff0f80e5947fe11d919fe322b29c36
macheteivan <44761500+macheteivan@users.noreply.github.com>
2018-11-05 20:33:21 +0800

    Add files via upload

---
+++ boxer[1].java
@@ -0,0 +1,25 @@
+package macheteivan;
+
+import exercise1.SingularlyLinkedList;
+
+
+public class boxer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingularlyLinkedList<String> boxers= new SingularlyLinkedList <String>();
+		
+		boxers.addFirst("BANING");
+		boxers.addFirst("BANTOT");
+		boxers.addFirst("BANSOY");
+		boxers.addFirst("BANGGI");
+		boxers.addFirst("BANOY");
+		
+		
+		
+		System.out.println("size : " + boxers.size());
+		
+	}
+
+}
+ 
